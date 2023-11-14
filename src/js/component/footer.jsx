aaeac4cfee0f18574d98cd4a7b61462ef304524a
Freddy2793 <51304209+Freddy2793@users.noreply.github.com>
2023-11-14 17:16:41 +0000

    landing page Freddy

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Footer = () => {
+
+    return (
+
+        <>
+
+        <div className="card-footer bg-dark" style={{color:"white"}}>
+
+            Freddy Gutierrez 2023
+
+        </div>
+
+
+        </>
+
+
+
+    )
+
+}
+
+export default Footer;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,22 +3,46 @@ import React from "react";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
+import Navbar from "./navbar.jsx";
+import Jumbotron from "./jumbotron.jsx";
+import Card from "./card.jsx";
+import Footer from "./footer.jsx";
+
 //create your first component
 const Home = () => {
 	return (
+
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			
+
+			<Navbar/>
+			<Jumbotron/>
+			<br/>
+			<div className="row">
+			<div className="col">
+
+			<Card imagen="https://s1.ppllstatics.com/lasprovincias/www/multimedia/202112/12/media/cortadas/gatos-kb2-U160232243326NVC-1248x770@Las%20Provincias.jpg" titulo="gato"/>
+			
+			</div>
+			<div className="col">
+				
+			<Card imagen="https://png.pngtree.com/background/20230611/original/pngtree-drawing-of-a-colorful-parrot-sitting-on-a-branch-picture-image_3172349.jpg" titulo="loro"/>
+			
+			</div>
+			<div className="col">
+				
+			<Card imagen="https://static.wikia.nocookie.net/wikia_tortuga/images/9/99/Tortuga-rusa.jpg/revision/latest/scale-to-width-down/800?cb=20200526211233&path-prefix=es" titulo="tortuga"/>
+			
+			</div>
+			<div className="col">
+				
+			<Card imagen="https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/09151144/Alaskan-Malamute-standing-in-the-grass1.jpg" titulo="Lobo"/>
+			
+			</div>
+			</div>
+			
+			<Footer/>
+
 		</div>
 	);
 };
